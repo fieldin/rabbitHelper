38324a8cf30a0802b18dbddc31a0d945f0736a73
ami <ami.malimovka@gmail.com>
2022-07-13 11:45:24 +0300

    added comments to the concurrent consumer

--- examples/consumerThread/main.go
+++ examples/consumerThread/main.go
@@ -13,22 +13,26 @@ import (
 
 // Here we show consuming messages from a queue in a different "thread": each consumer runs in its own go routine
 func main() {
+	// listen to os signals (CTRL-C, kill, etc)
 	interrupt := make(chan os.Signal)
 	defer close(interrupt)
 	signal.Notify(interrupt, syscall.SIGTERM, os.Interrupt, os.Kill)
 
+	// this channel will be used to notify the consumers that they should exist
 	done := make(chan bool)
 
+	// random number generator: to distinguish between the consumers
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 
+	// start several consumers, each with its own id
 	go consumerLoop(done, r1.Intn(100))
 	go consumerLoop(done, r1.Intn(100))
 
 	fmt.Println("I have moved on to other stuff")
-	fmt.Println("waiting to CTRL-C")
+	fmt.Println("waiting for CTRL-C")
 	<-interrupt
-	close(done)
+	close(done) // closing the done channel will signal the consumers to exit
 	time.Sleep(50 * time.Millisecond)
 }
 
@@ -52,12 +56,14 @@ func consumerLoop(done chan bool, consumerId int) {
 		log.Fatal("failed consuming incoming messages")
 	}
 
+	// exit message
 	defer func() {
 		fmt.Println(consumerName, " is closing")
 	}()
+
 	for {
 		select {
-		case <-done:
+		case <-done: // if we're done - then leave
 			return
 		case rm := <-rbt.MessagesChannel:
 			fmt.Println(consumerName, " <- ", string(rm.Body))
